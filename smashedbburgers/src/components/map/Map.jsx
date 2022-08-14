e056a7bf49ce10ce7d026594f8b2f580e7fd1577
Fritzcho <alireza.ramezani@weknowit.se>
2022-08-14 23:21:09 +0200

    New changes

--- smashedbburgers/src/components/map/Map.jsx
+++ smashedbburgers/src/components/map/Map.jsx
@@ -11,8 +11,10 @@ const Map = () => {
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1758.9733063853628!2d20.23622547403796!3d63.840882591688384!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x467c5b66613016e9%3A0x9ba75dba31a2e674!2zRsO2cnLDpWRzdsOkZ2VuIDEzLCA5MDEgMzIgVW1lw6U!5e0!3m2!1ssv!2sse!4v1657537382958!5m2!1ssv!2sse"
             height="450" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade" className='sbb__map-map'></iframe>
             <div className='sbb__map-info'>
-                <p>Måndag till lördag</p>
-                <p>10:00 - 14:00</p>
+                <p>Måndag till Lördag:</p>
+                <p>10:00 - 13:30</p>
+                <br/>
+                <p>Söndag: Stängt</p>
             </div>
          </div>
     </div>
